b6e3f7e4704e0372be5885306873fae4fe17ef39
Uzias Rivera <uziasrivera@gmail.com>
2019-10-07 15:07:08 -0700

    added remove feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+
+//context
 import {ProductContext} from './contexts/ProductContext'
+import {CartContext} from './contexts/CartContext'
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
 
 function App() {
 	const [products] = useState(data);
@@ -14,22 +18,41 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
-		setCart([...cart, item])
+		setCart([...cart,item])
+		// if(cart.length===0){
+		// 	setCart([...cart,item])
+
+		// }else{
+
+		// cart.forEach(oldItem=>{
+		// 	if (item.id===oldItem.id){
+		// 		console.log('this is already in here')
+		// 	}else{
+		// 		setCart([...cart,item])
+		// 	}
+			
+		// })}
 		// console.log(item)
 	};
+	const removeItem = id => {
+		console.log(cart)
+        setCart(cart.filter(remove=> remove.id !== id))
+    }
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{cart, removeItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<ProductContext.Provider value={{ products, addItem }}>
 			<Route exact path="/" component={Products} />
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
